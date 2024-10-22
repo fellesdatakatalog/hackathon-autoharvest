f730f1ec44f5565fbbcb0b1d837c14a96ae41f65
halvor <halvorbmundal@gmail.com>
2024-10-22 13:43:15 +0200

    Remove test folder

--- src/test/kotlin/no/digdir/accessrequestapi/utils/ApiTestContext.kt
+++
@@ -1,8 +0,0 @@
-package no.digdir.accessrequestapi.utils
-
-import org.springframework.boot.test.web.server.LocalServerPort
-
-abstract class ApiTestContext {
-    @LocalServerPort
-    var port = 0
-}

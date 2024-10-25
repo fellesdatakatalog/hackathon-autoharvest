e79038db6c4c0b3b0b2c49ceffea6f52885a1448
Hans Olav Sund <hans.olav.sund@digdir.no>
2024-10-25 10:46:17 +0200

    chore: test1

--- src/main/kotlin/no/digdir/hackathontest/Application.kt
+++ src/main/kotlin/no/digdir/hackathontest/Application.kt
@@ -1,4 +1,4 @@
-package no.digdir.accessrequestapi
+package no.digdir.hacakathontest
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
